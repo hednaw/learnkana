b29869046dded6ec1972a2e6ee2e915cf1bbee4c
hednaw <hednaw0@protonmail.com>
2023-05-01 22:43:52 +0200

    merged two tables into one with an object with 2 values

--- main.js
+++ main.js
@@ -1,97 +1,51 @@
 const Characters = [
-  "あ",
-  "い",
-  "う",
-  "え",
-  "お",
-  "か",
-  "き",
-  "く",
-  "け",
-  "こ",
-  "さ",
-  "し",
-  "す",
-  "せ",
-  "そ",
-  "た",
-  "ち",
-  "つ",
-  "て",
-  "と",
-  "な",
-  "に",
-  "ぬ",
-  "ね",
-  "の",
-  "は",
-  "ひ",
-  "ふ",
-  "へ",
-  "ほ",
-  "ま",
-  "み",
-  "む",
-  "め",
-  "も",
-  "や",
-  "ゆ",
-  "よ",
-  "ら",
-  "り",
-  "る",
-  "れ",
-  "ろ",
-  "わ",
-  "を",
+  {char: "あ", romanji: "a"},
+  {char: "い", romanji: "i"},
+  {char: "う", romanji: "u"},
+  {char:"え", romanji: "e"},
+  {char: "お", romanji: "o"},
+  {char:"か",romanji:"ka"},
+  {char: "き", romanji: "ki"},
+  {char:"く",romanji: "ku"},
+  {char:"け",romanji:"ke"},
+  {char: "こ",romanji:"ko"},
+  {char: "さ",romanji:"sa"},
+  {char:"し",romanji:"shi"},
+  {char:"す",romanji:"su"},
+  {char:"せ",romanji:"se"},
+  {char:"そ",romanji:"so"},
+  {char:"た",romanji:"ta"},
+  {char: 'ち',romanji: 'chi'},
+  {char: 'つ',romanji: 'tsu'},
+  {char: 'て',romanji: 'te'},
+  {char: 'と',romanji: 'to'},
+  {char: "な",romanji: 'na'},
+  {char: "に",romanji: 'ni'},
+  {char: "ぬ",romanji: 'nu'},
+  {char: "ね",romanji: 'ne'},
+  {char: "の", romanji: "no"},
+  {char: "は",romanji: 'ha'},
+  {char: "ひ",romanji: 'hi'},
+  {char: "ふ",romanji: 'fu'},
+  {char: "へ",romanji: 'he'},
+  {char: "ほ",romanji: 'ho'},
+  {char: "ま",romanji: 'ma'},
+  {char: "み",romanji: 'mi'},
+  {char: "む",romanji: 'mu'},
+  {char: "め",romanji: 'me'},
+  {char: "も",romanji: 'mo'},
+  {char:"や",romanji:'ya'},
+  {char:"ゆ",romanji:'yu'},
+  {char:"よ",romanji:'yo'},
+  {char: "ら",romanji: 'ra'},
+  {char: "り",romanji: 'ri'},
+  {char: "る",romanji: 'ru'},
+  {char: "れ",romanji: 're'},
+  {char: "ろ",romanji: 'ro'},
+  {char: "わ",romanji: 'wa'},
+  {char: "を",romanji: 'wo'},
 ];
-const Translated = [
-  "a",
-  "i",
-  "u",
-  "e",
-  "o",
-  "ka",
-  "ki",
-  "ku",
-  "ke",
-  "ko",
-  "sa",
-  "shi",
-  "su",
-  "se",
-  "so",
-  "ta",
-  "chi",
-  "tsu",
-  "te",
-  "to",
-  "na",
-  "ni",
-  "nu",
-  "ne",
-  "no",
-  "ha",
-  "hi",
-  "fu",
-  "he",
-  "ho",
-  "ma",
-  "mi",
-  "mu",
-  "me",
-  "mo",
-  "ya",
-  "yu",
-  "yo",
-  "ra",
-  "ri",
-  "ru",
-  "re",
-  "ro",
-  "wa",
-  "wo",
-];
+
 const characterLabel = document.getElementById("character");
 const checkButton = document.getElementById("check");
 const answerInput = document.getElementById("input");
@@ -101,18 +55,15 @@ let streak = 0;
 let funnyFlag = false;
 
 function randomize() {
-  streakLabel.innerHTML = "Streak: " + streak;
-  infoText.innerHTML = "";
+  streakLabel.innerText = "Streak: " + streak;
+  infoText.innerText = "";
   answerInput.value = "";
   answerInput.style.backgroundColor = "#5b6078";
   currentCharacter = Math.floor(Math.random() * Characters.length);
-  console.log(Characters[currentCharacter]);
-  console.log(currentCharacter);
-  characterLabel.textContent = Characters[currentCharacter];
+  characterLabel.textContent = Characters[currentCharacter].char;
   for (let i = 0; i < Characters.length; i++) {
-    console.log(Characters[i], Translated[i]);
+    console.log(Characters[i].char,Characters[i].romanji);
   }
-  console.log(Characters.length, Translated.length);
 }
 
 randomize();
@@ -128,19 +79,15 @@ checkButton.addEventListener("click", function () {
     checkButton.innerText = "Check";
     randomize();
   } else {
-    if (answerInput.value == Translated[currentCharacter]) {
+    if (answerInput.value == Characters[currentCharacter].romanji) {
       console.log("Good");
       answerInput.style.backgroundColor = "#a6da95";
       funnyFlag = true;
       checkButton.innerText = "Next";
       streak++;
     } else {
-      console.log(
-        "That's not it. Correct answer is: ",
-        Translated[currentCharacter]
-      );
       infoText.innerHTML =
-        "That's not it. Correct answer is: " + Translated[currentCharacter];
+        "That's not it. Correct answer is: " + Characters[currentCharacter].romanji;
       answerInput.style.backgroundColor = "#ee99a0";
       funnyFlag = true;
       checkButton.innerText = "Next";
